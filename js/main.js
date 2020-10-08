35938bd18927e70ef2f5f398a17260fec327f8f4
web-punch <korotkov_alexey@bk.ru>
2020-10-09 00:17:09 +0300

    create: added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var mySwiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  loop: true,
+  keyboard: {
+    enabled: true,
+    pageUpDown: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-button--next",
+    prevEl: ".slider-button--prev",
+  },
+
+  effect: "coverflow",
+});
